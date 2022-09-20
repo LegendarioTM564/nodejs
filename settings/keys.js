cadaef02eeb9b46e71590a227784f5f6fe91a39f
Juan Bravo <bravojuan564@gmail.com>
2022-09-20 14:36:22 -0300

    bugs

--- settings/keys.js
+++ settings/keys.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv').config()
 module.exports = {
     dataBase:{
     host:process.env.DATABASE_HOST,
-    port:process.env.DATABASE_PORT,
     user:process.env.DATABASE_USER,
     password:process.env.DATABASE_PASSWORD,
     database:process.env.DATABASE
